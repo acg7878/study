9031bc8eab7bd97e6a2c1f53fdca5858d7093156
s_yangjunke <s_yangjunke@wps.cn>
2025-11-18 11:39:23 +0800

    feat: 删除内容，再次实现

--- data_structure/tree/AVL/AVLTree.h
+++ data_structure/tree/AVL/AVLTree.h
@@ -1,45 +1,45 @@
 #ifndef AVLTREE_H
 #define AVLTREE_H
 
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 
 struct AVLNode {
-    int key;
-    AVLNode* left;
-    AVLNode* right;
-    int height;
+  int key;
+  AVLNode* left;
+  AVLNode* right;
+  int height;
 
-    AVLNode(int value) : key(value), left(nullptr), right(nullptr), height(1) {}
+  AVLNode(int value) : key(value), left(nullptr), right(nullptr), height(1) {}
 };
 
 class AVLTree {
-public:
-    AVLTree();
-    ~AVLTree();
+ public:
+  AVLTree();
+  ~AVLTree();
 
-    void insert(int key);
-    void remove(int key);
-    AVLNode* search(int key);
-    void inorderTraversal();
+  void insert(int key);
+  void remove(int key);
+  AVLNode* search(int key);
+  void inorderTraversal();
 
-private:
-    AVLNode* root;
+ private:
+  AVLNode* root;
 
-    int getHeight(AVLNode* node);
-    int getBalance(AVLNode* node);
-    void updateHeight(AVLNode* node);
+  int getHeight(AVLNode* node);
+  int getBalance(AVLNode* node);
+  void updateHeight(AVLNode* node);
 
-    AVLNode* rightRotate(AVLNode* y);
-    AVLNode* leftRotate(AVLNode* x);
+  AVLNode* rightRotate(AVLNode* y);
+  AVLNode* leftRotate(AVLNode* x);
 
-    AVLNode* insertNode(AVLNode* node, int key);
-    AVLNode* removeNode(AVLNode* node, int key);
-    AVLNode* findMin(AVLNode* node);
+  AVLNode* insertNode(AVLNode* node, int key);
+  AVLNode* removeNode(AVLNode* node, int key);
+  AVLNode* findMin(AVLNode* node);
 
-    AVLNode* searchNode(AVLNode* node, int key);
-    void inorder(AVLNode* node);
-    void destroyTree(AVLNode* node);
+  AVLNode* searchNode(AVLNode* node, int key);
+  void inorder(AVLNode* node);
+  void destroyTree(AVLNode* node);
 };
 
-#endif // AVLTREE_H
+#endif  // AVLTREE_H
