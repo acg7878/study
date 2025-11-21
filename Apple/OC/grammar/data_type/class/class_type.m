d08c5dc1b6d6b00ab991d4c8816c467dea1e63ee
macbook_air <2477952281@qq.com>
2025-11-21 22:26:43 +0800

    feat(oc): 优化Class类型代码示例

--- Apple/OC/grammar/data_type/class/class_type.m
+++ Apple/OC/grammar/data_type/class/class_type.m
@@ -1,10 +1,18 @@
-#import <Foundation/Foundation.h>
 #import "Person.h"
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
 
 int main() {
-    @autoreleasepool {
-        Class person;
-        person = [Person class];
-        NSLog(@"%@",person);
-    }
+  @autoreleasepool {
+    Class person = [Person class];
+    Class strClass = [NSString class];
+    NSString *str = @"Hello";
+    Class strClass2 = object_getClass(str);
+    Class strClass3 = object_getClass([NSString class]);
+
+    NSLog(@"person: %@", person);
+    NSLog(@"strClass: %@", strClass);
+    NSLog(@"strClass2: %@", strClass2);
+    NSLog(@"strClass3: %@", strClass3);
+  }
 }
