68a1a83a8dc1e2386d55f57b4483737b6af2ba7e
macbook_air <2477952281@qq.com>
2025-11-21 23:45:21 +0800

    feat(swift): 添加注释

--- Apple/swift/basic/2.control_flow/5_guard.swift
+++ Apple/swift/basic/2.control_flow/5_guard.swift
@@ -9,3 +9,10 @@ func printStr(name: String?) {
 
 printStr(name: "man")
 printStr(name: nil)
+
+// 与 if let相比
+// 更清晰的语义：明确地表达了 “前置条件” 的概念。
+// 更安全的保障：编译器强制要求处理不满足条件的情况。
+
+// 更便捷的变量作用域：让绑定的变量可以在后续代码中直接使用。
+// 因为if let xx {} 这样创建的变量只能存在于{}
