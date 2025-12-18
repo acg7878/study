b1026b741feb838217ef2cdca464d54476f30d83
macbook_air <2477952281@qq.com>
2025-12-18 16:22:24 +0800

    feat(backtracking): 实现组合生成算法

---
+++ algorithm/hot100/backtracking/combine/1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<int>> ans;
+  vector<int> path;
+  vector<vector<int>> combine(int n, int k) {
+    backtracking(1, n, k);
+    return ans;
+  }
+
+  void backtracking(int index, int n, int k) {
+    if (path.size() == k) {
+      ans.push_back(path);
+      return;
+    }
+
+    for (int i = index; i <= n; ++i) {
+      path.push_back(i);
+      backtracking(i + 1, n, k);
+      path.pop_back();
+    }
+  }
+};
+
+int main() {
+  Solution s;
+  vector<vector<int>> ans = s.combine(4, 2);
+  for (auto nums : ans) {
+    for (auto num : nums) {
+      cout << num << " ";
+    }
+    cout << "\n";
+  }
+}
