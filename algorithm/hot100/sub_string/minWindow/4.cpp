46953f075d202f2df43a8cf4b168e44cb39e47df
yangjunke <yangjunke@baidu.com>
2026-01-13 21:07:04 +0800

    feat(min-window-substring): 添加最小覆盖子串算法实现

---
+++ algorithm/hot100/sub_string/minWindow/4.cpp
@@ -0,0 +1,43 @@
+#include <climits>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+ public:
+  string minWindow(string s, string t) {
+    int s_size = s.size();
+    int cnt = t.size();
+    unordered_map<char, int> um;  // 字符：字符数
+    for (char a : t) {
+      um[a]++;
+    }
+
+    int start_index = 0;
+    int min_len = INT_MAX;
+
+    int left = 0, right = 0;
+    while (right < s_size) {
+      // 向前移动，消费一下字符
+      if (um[s[right]] > 0) {
+        cnt--;
+      }
+      um[s[right]]--;
+
+      if (right - left + 1 < min_len) {
+        start_index = left;
+        min_len = right - left + 1;
+      }
+
+      // 尝试收缩
+      while (cnt == 0) {
+        if (um[s[left]] == 0) {
+            cnt++;
+        }
+        um[s[left]]++;
+        left++;
+      }
+      right++;
+    }
+  }
+};
