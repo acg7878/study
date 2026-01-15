120d594ca9e414d72e0eb2d2fe8add23dc159b34
yangjunke <yangjunke@baidu.com>
2026-01-15 23:35:27 +0800

    feat(unique-paths): 添加独特路径算法实现
    Change-Id: Ifacc24c9bfe697ca698eae74a3fb740f6aeacdaa

---
+++ algorithm/hot100/dp/unique-paths/ans.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+/*
+    链接：https://leetcode.cn/problems/unique-paths/
+    思路：
+        1. 动态规划
+        2. dp[i][j] 表示从(0,0)出发到(i,j)有多少条不同的路径
+        3. 状态转移方程：dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        4. 初始化：第一行和第一列只能通过一直向右或一直向下到达，路径数为1
+*/
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1; // 第一行和第一列只能直达
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        
+        return dp[m - 1][n - 1];
+    }
+};
