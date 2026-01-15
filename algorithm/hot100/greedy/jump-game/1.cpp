3f862493329911b42259d45d2b263c80fc9f1192
yangjunke <yangjunke@baidu.com>
2026-01-15 23:02:32 +0800

    feat(jumpGame): 添加跳跃游戏算法实现
    Change-Id: If1fb0e58c8e5d80897325b835edb4c678d43bdeb

---
+++ algorithm/hot100/greedy/jump-game/1.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int cover = 0;
+        for (int i = 0; i <= cover; ++i) {
+            cover = max(cover, i + nums[i]);
+            if (cover >= nums.size() - 1) return true;
+        }
+        return false;
+    }
+};
