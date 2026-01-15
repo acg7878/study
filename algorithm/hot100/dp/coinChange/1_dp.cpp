76e0400790baa4195b1c4c45d4819debdd25e73e
yangjunke <yangjunke@baidu.com>
2026-01-15 17:15:19 +0800

    feat(coinChange): dp尝试，还是不太会
    Change-Id: I0ad13f5811c8bb7bdf432c53740de4edcb85c237

---
+++ algorithm/hot100/dp/coinChange/1_dp.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+
+#include <algorithm>
+
+class Solution {
+ public:
+  int coinChange(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, amount + 1);
+    dp[0] = 0;
+    for (int i = 1; i <= amount; ++i) {
+      for (int coin : coins) {
+        if (i >= coin) {
+            // 使用这枚硬币是否更省次数？
+            // 是的话就将状态数组中 凑i-coin需要的硬币数 +1
+          dp[i] = min(dp[i], dp[i - coin] + 1);
+        }
+      }
+    }
+  }
+};
